f86a28a9d9f49f3c1f22bc3397f27d10a066e4a6
Vadim Tsvang <rsb7@rambler.ru>
2022-05-12 12:31:45 +0300

    initial commit

---
+++ Navigation/Post/PostViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PostViewController.swift
+//  Navigation
+//
+//  Created by Vadim on 03.02.2022.
+//
+
+import UIKit
+import StorageService
+
+class PostViewController: UIViewController {
+    
+    var post: Post?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // title = post?.title
+        view.backgroundColor = .brown
+        
+        let barButton = UIBarButtonItem(barButtonSystemItem: .organize, target: self, action: #selector(tapInfo))
+        navigationItem.rightBarButtonItem = barButton
+    }
+    
+    @objc func tapInfo() {
+        let vcInfo = InfoViewController()
+        present(vcInfo, animated: true, completion: nil)
+    }
+}
+
---
+++ Navigation/UIView+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIView+Extension.swift
+//  Navigation
+//
+//  Created by Vadim on 08.03.2022.
+//
+
+import UIKit
+import StorageService
+
+public extension UIView {
+
+    func toAutoLayout() {
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    func addSubviews(_ subviews: UIView...) {
+        subviews.forEach { addSubview($0) }
+    }
+}
